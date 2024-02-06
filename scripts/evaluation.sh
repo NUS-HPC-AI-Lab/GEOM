e7b8ed0b0b6751ddd31bc49559e957046a478eaf
Yuchen_Zhang <yuchen.zhang2003@gmail.com>
2024-02-06 20:33:40 +0800

    Update evaluation.sh

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -1,1 +1,72 @@
+# OGB 71.4
+
+# python eval_condg.py --section ogbn-arxiv-r01-DATM
+# TEST: Full Graph Mean Accuracy: 0.713495, STD: 0.000909
+
+# python eval_condg.py --section ogbn-arxiv-r005-DATM
+# TEST: Full Graph Mean Accuracy: 0.710162, STD: 0.001869
+
+# python eval_condg.py --section ogbn-arxiv-r001-DATM
+# TEST: Full Graph Mean Accuracy: 0.696179, STD: 0.002327
+
+# python eval_condg.py --section ogbn-arxiv-r0005-DATM
+# TEST: Full Graph Mean Accuracy: 0.687711, STD: 0.001516
+
+# python eval_condg.py --section ogbn-arxiv-r0001-DATM
+# TEST: Full Graph Mean Accuracy: 0.655132, STD: 0.005791
+
+# reddit
+# python eval_condg_tem.py --section reddit-r0125-DATM
+# TEST: Full Graph Mean Accuracy: 0.938457, STD: 0.000145
+
+# python eval_condg.py --section reddit-r0075-DATM
+# TEST: Full Graph Mean Accuracy: 0.937341, STD: 0.000443
+
+# python eval_condg.py --section reddit-r0005-DATM
+# TEST: Full Graph Mean Accuracy: 0.914834, STD: 0.000859
+
+# python eval_condg.py --section reddit-r0001-DATM
+# TEST: Full Graph Mean Accuracy: 0.914288, STD: 0.000378
+
+# python eval_condg.py --section reddit-r00005-DATM
+# TEST: Full Graph Mean Accuracy: 0.911367, STD: 0.002153
+
+# citeseer
+
+# python eval_condg.py --section citeseer-r025-DATM
+# TEST: Full Graph Mean Accuracy: 0.729800, STD: 0.005016
+
+# python eval_condg.py --section citeseer-r05-DATM
+# TEST: Full Graph Mean Accuracy: 0.742900, STD: 0.001446
+
+# python eval_condg.py --section citeseer-r1-DATM
+# TEST: Full Graph Mean Accuracy: 0.732600, STD: 0.004409
+
+# cora
+
+# python eval_condg.py --section cora-r025-DATM
+# TEST: Full Graph Mean Accuracy: 0.824700, STD: 0.004196
+
+# python eval_condg.py --section cora-r05-DATM
+# TEST: Full Graph Mean Accuracy: 0.836200, STD: 0.002676
+
+# python eval_condg.py --section cora-r1-DATM
+# TEST: Full Graph Mean Accuracy: 0.828400, STD: 0.007102
+
+# flickr
+
+# python eval_condg.py --section flickr-r001-DATM
+# TEST: Full Graph Mean Accuracy: 0.472819, STD: 0.000851
+
+# python eval_condg.py --section flickr-r0005-DATM
+# TEST: Full Graph Mean Accuracy: 0.470416, STD: 0.001856
+
+# python eval_condg.py --section flickr-r0001-DATM
+# TEST: Full Graph Mean Accuracy: 0.471115, STD: 0.003163
+
+
+
+
+
+
 
